d27e197b354dc086ce6c4e97831284c0ba0b1b89
Ralf Lackinger <ralf.lackinger@anevis-solutions.com>
2024-02-20 11:05:31 +0100

    sync source code

--- src/main/java/me/lackinger/edgeservice/web/WebEndpoints.java
+++ src/main/java/me/lackinger/edgeservice/web/WebEndpoints.java
@@ -1,9 +1,8 @@
 package me.lackinger.edgeservice.web;
 
-import static org.springframework.http.HttpStatus.SERVICE_UNAVAILABLE;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -15,9 +14,10 @@ public class WebEndpoints {
 	@Bean
 	public RouterFunction<ServerResponse> routerFunction() {
 		return RouterFunctions.route()
-		                      .GET("/catalog-fallback",
-				                      request -> ServerResponse.ok().body(Mono.just(""), String.class))
-		                      .POST("/catalog-fallback", request -> ServerResponse.status(SERVICE_UNAVAILABLE).build())
+		                      .GET("/catalog-fallback", request ->
+				                      ServerResponse.ok().body(Mono.just(""), String.class))
+		                      .POST("/catalog-fallback", request ->
+				                      ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE).build())
 		                      .build();
 	}
 
