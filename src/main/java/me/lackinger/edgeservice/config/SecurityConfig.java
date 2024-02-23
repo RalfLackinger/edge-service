cf0aa82efa0c4efad061f273039cab6f2d6f3ed8
Ralf Lackinger <ralf.lackinger@anevis-solutions.com>
2024-02-23 12:31:49 +0100

    sync code

--- src/main/java/me/lackinger/edgeservice/config/SecurityConfig.java
+++ src/main/java/me/lackinger/edgeservice/config/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.server.WebSessionServerOAuth2AuthorizedClientRepository;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
@@ -18,15 +20,21 @@ import org.springframework.security.web.server.csrf.XorServerCsrfTokenRequestAtt
 import org.springframework.web.server.WebFilter;
 import reactor.core.publisher.Mono;
 
-@EnableWebFluxSecurity
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
+
+	@Bean
+	ServerOAuth2AuthorizedClientRepository authorizedClientRepository() {
+		return new WebSessionServerOAuth2AuthorizedClientRepository();
+	}
 
 	@Bean
 	SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http,
 			ReactiveClientRegistrationRepository clientRegistrationRepository) {
 		return http
 				.authorizeExchange(exchange -> exchange
+						.pathMatchers("/actuator/**").permitAll()
 						.pathMatchers("/", "/*.css", "/*.js", "/favicon.ico").permitAll()
 						.pathMatchers(HttpMethod.GET, "/books/**").permitAll()
 						.anyExchange().authenticated()
@@ -37,7 +45,7 @@ public class SecurityConfig {
 				.logout(logout -> logout.logoutSuccessHandler(oidcLogoutSuccessHandler(clientRegistrationRepository)))
 				.csrf(csrf -> csrf
 						.csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse())
-						.csrfTokenRequestHandler(new XorServerCsrfTokenRequestAttributeHandler()))
+						.csrfTokenRequestHandler(new XorServerCsrfTokenRequestAttributeHandler()::handle))
 				.build();
 	}
 
